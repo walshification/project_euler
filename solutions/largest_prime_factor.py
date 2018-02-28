af3a3a6fa697e875a7074ff03c4057033cec3a6d
Christopher Walsh <walshification@gmail.com>
2018-02-28 08:34:59 -0600

    finds largest prime factor of n

---
+++ solutions/largest_prime_factor.py
@@ -0,0 +1,36 @@
+'''
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+'''
+
+
+import math
+
+
+def find_largest_of_number(number):
+    return [prime for prime in generate_prime_factors(number)][-1]
+
+
+def generate_prime_factors(number):
+    factor_limit = math.ceil(math.sqrt(number)) + 1
+    prime_factors = [2]
+    composites = {}
+
+    for prime in range(3, factor_limit, 2):
+        if not composites.get(prime):
+            for composite in generate_composites(prime, factor_limit):
+                composites[composite] = True
+            if number % prime == 0:
+                prime_factors.append(prime)
+
+    return prime_factors
+
+
+def generate_composites(base, limit):
+    for i in range(base, limit + 1, base):
+        yield i
+
+
+if __name__ == '__main__':
+    print(find_largest_of_number(600851475143))  # => 6857
